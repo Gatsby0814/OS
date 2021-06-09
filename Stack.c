026a4c51f0c6481510d639cb276b0bd9744c2c7b
unknown <2018121152@yonsei.ac.kr>
2021-06-09 16:50:57 +0900

    Stack add

---
+++ Stack.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAX 255
+
+void add(int a);
+void pop();
+void list();
+int num();
+
+int* Stack = NULL;
+
+int main(){
+    
+        return 0;
+}
+
+void add(int a){
+
+
+
+}
+
+void list(){
+
+    printf("element num : %d\n", num);
+    printf("elements : ");
+    
+    i = 0;
+        
+
+}
+
+int num(){
+    int num = 0; 
+    int i = 0;
+    
+    while(Stack[i]!=NULL){
+        num++;
+        i++;
+    }
+    return num;
+}
+
