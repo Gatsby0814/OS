f8aad9bbab33f368d4b68a010258ec2f9c3e3d65
Your Name beefmushroom <hamburgerisperfectfood@gmail.com>
2021-06-11 14:15:12 +0900

    test

---
+++ hello/hello/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello
+//
+//  Created by 김성준 on 2021/06/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
